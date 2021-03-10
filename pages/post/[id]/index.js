16343b5c8b51c133b0fe91bc32fbed5806669583
Dave Beshero <dbesher@us.ibm.com>
2021-03-10 11:07:50 -0500

    maybe

--- pages/post/[id]/index.js
+++ pages/post/[id]/index.js
@@ -1,13 +1,29 @@
-import { useRouter } from 'next/router'
-import Link from 'next/link'
-import Header from '../../../components/header'
+import { useRouter } from "next/router";
+import Link from "next/link";
+import Header from "../../../components/header";
+import Head from "next/head";
 
 const Post = () => {
-  const router = useRouter()
-  const { id } = router.query
+  const router = useRouter();
+  const { id } = router.query;
+  const description = "My pose with Spot";
+  const pageTitle = "formant spot teleop";
 
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta name="description" content={description}></meta>
+        <meta property="og:title" content={pageTitle} key="ogtitle" />
+        <meta property="og:description" content={description} key="ogdesc" />
+        <meta
+          property="og:video"
+          content={`https://geppetto-clips.formant.io/${id}.mp4`}
+          key="ogdesc"
+        />
+        <title>{pageTitle}</title>
+      </Head>
       <Header />
       <h1>Post: {id}</h1>
       <ul>
@@ -23,7 +39,7 @@ const Post = () => {
         </li>
       </ul>
     </>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
