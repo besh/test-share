88fd2633c69f42c88dc9bb259dd37af5245241d8
Dave Beshero <dbesher@us.ibm.com>
2021-03-10 11:48:41 -0500

    meta meta meta

--- pages/post/[id]/index.js
+++ pages/post/[id]/index.js
@@ -11,15 +11,19 @@ const Post = ({ id }) => {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet="utf-8" />
-        <meta name="description" content={description}></meta>
-        <meta name="og:title" content={pageTitle} key="ogtitle" />
-        <meta name="og:description" content={description} key="ogdesc" />
         <meta
-          name="og:video"
+          property="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+        <meta charSet="utf-8" />
+        <meta property="description" content={description}></meta>
+        <meta property="og:title" content={pageTitle} key="ogtitle" />
+        <meta property="og:type" content="video.movie" />
+        <meta property="og:site_name" content="Formant" />
+        <meta property="og:description" content={description} key="ogdesc" />
+        <meta
+          property="og:video"
           content={`https://geppetto-clips.formant.io/${id}.mp4`}
-          key="ogdesc"
         />
         <title>{pageTitle}</title>
       </Head>
